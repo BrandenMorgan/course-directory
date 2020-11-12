9196ace707265f422c596df013feccda959e742b
Branden Morgan <branden.morgan@gmail.com>
2020-11-12 12:30:04 -0800

    Updates

---
+++ precache-manifest.8303271499e8895e99de3d55986292c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78304b51145594338118",
+    "url": "/course-directory/static/css/main.94d5dbd9.chunk.css"
+  },
+  {
+    "revision": "78304b51145594338118",
+    "url": "/course-directory/static/js/main.78304b51.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "da9058c68526cb74fbf1f941aad4e256",
+    "url": "/course-directory/index.html"
+  }
+];
